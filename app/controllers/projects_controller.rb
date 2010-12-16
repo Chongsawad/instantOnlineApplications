8f582c12d3b079a7de534276acfa2614785c7ec8
Chongsawad Saiaram <Chongsawad252@msn.com>
2010-12-16 23:22:28 +0700

    newSearchForm

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.xml
   def index
-    @projects = Project.all
+    @projects = Project.search(params[:search])
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,12 @@ class Project < ActiveRecord::Base
   has_many :users, :through => "sites"
   
   has_attached_file :screenshot, :styles => { :showcase => "272x296>" }
+  
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
 end
